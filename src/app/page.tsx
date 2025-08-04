dd116e2bcbd75c8c2bbd377a4c42f8cc3e4ffb45
Timothy Roy <timothy.roy@pm.me>
2025-08-04 14:02:42 -0500

    Add shadcn button to test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -49,6 +50,13 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <Button
+            className="w-full sm:w-auto"
+            variant="default"
+            size="lg"
+          >
+            Hello world
+          </Button>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
